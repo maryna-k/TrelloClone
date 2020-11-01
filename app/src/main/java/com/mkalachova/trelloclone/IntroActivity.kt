c6f57873159f11d2b3c21c00d39ef68c02302f53
Maryna Kalachova <kalachova.maryna@gmail.com>
2020-11-01 13:07:01 -0500

    add IntroActivity

---
+++ app/src/main/java/com/mkalachova/trelloclone/IntroActivity.kt
@@ -0,0 +1,17 @@
+package com.mkalachova.trelloclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+    }
+}
--- app/src/main/java/com/mkalachova/trelloclone/SplashActivity.kt
+++ app/src/main/java/com/mkalachova/trelloclone/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.mkalachova.trelloclone
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager
 import kotlinx.android.synthetic.main.activity_splash.*
 
@@ -18,5 +20,10 @@ class SplashActivity : AppCompatActivity() {
 
         val font: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
         tv_app_name.typeface = font
+
+        Handler().postDelayed({
+            startActivity(Intent(this, IntroActivity::class.java))
+            finish()
+        }, 2000)
     }
 }
